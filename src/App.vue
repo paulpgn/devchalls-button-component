564c57664055faf77e41dc939dac3172bcc4e1fc
paulpgn <105318234+paulpgn@users.noreply.github.com>
2022-05-12 13:26:42 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Btn from './components/Btn.vue'
+</script>
+
+
+<template>
+<Btn class="baseButton"/>
+<Btn class="outlined" />
+<Btn class="text"/>
+<Btn class="disableShadow"/>
+<Btn class="disabled" disabled buttonText="Disabled"/>
+<Btn class="disabledText" disabled buttonText="Disabled"/>
+
+</template>
+
+
+
+<style>
+
+</style>
---
+++ src/components/Btn.vue
@@ -0,0 +1,130 @@
+<template>
+<button class="[outlined,baseButton]"> <img :src="iconBefore"/> {{buttonText}} <img :src="iconAfter"/> </button>
+</template>
+
+<script>
+export default {
+    props: {
+        
+        buttonText: {
+            type: String,
+            default: () => "Default",
+        },
+        buttonIcon: {
+            type: Image,
+        },
+        baseButton: {
+            type: Boolean,
+            default: () => true,
+        },
+        outlined: {
+            type: Boolean,
+            default: () => false,
+        },
+    },
+};
+</script>
+
+<style scoped>
+
+.baseButton {
+    position: absolute;
+    width: 81px;
+    height: 36px;
+    left: 319px;
+    top: 141px;
+    border:none;
+    background: #E0E0E0;
+    box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+    border-radius: 6px;
+}
+.baseButton:hover,.baseButton:focus {
+    position: absolute;
+    width: 81px;
+    height: 36px;
+    left: 319px;
+    top: 141px;
+
+    background: #AEAEAE;
+    box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+    border-radius: 6px; 
+}
+.outlined {
+    position: absolute;
+    width: 88px;
+    height: 36px;
+    left: 319px;
+    top: 246px;
+    color:#3D5AFE;
+    background-color: white;
+    border: 1px solid #3D5AFE;
+    border-radius: 6px;
+}
+
+.outlined:hover,.outlined:focus {
+    position: absolute;
+    width: 88px;
+    height: 36px;
+    left: 319px;
+    top: 246px;
+    background: rgba(41, 98, 255, 0.1);
+    border: 1px solid #3D5AFE;
+    border-radius: 6px;
+}
+
+.text {
+    position: absolute;
+    color:blue;
+    width: 88px;
+    height: 36px;
+    left: 319px;
+    top: 351px;
+    border:none;
+    background: white;
+    border-radius: 6px;
+}
+
+.text:hover,.text:focus {
+    position: absolute;
+    color:blue;
+    width: 88px;
+    height: 36px;
+    left: 319px;
+    top: 351px;
+    border:none;
+    background: rgba(41, 98, 255, 0.1);
+    border-radius: 6px;
+}
+.disableShadow {
+    position: absolute;
+    width: 81px;
+    height: 36px;
+    left: 319px;
+    top: 453px;
+    color:white;
+    border:none;
+    background: #3D5AFE;
+    border-radius: 6px;
+}
+.disabled {
+    position: absolute;
+    width: 88px;
+    height: 36px;
+    left: 319px;
+    top: 558px;
+    border:none;
+    background: #E0E0E0;
+    border-radius: 6px;
+}
+.disabledText {
+    position: absolute;
+    width: 62px;
+    height: 20px;
+    left: 558px;
+    top: 566px;
+    color: #9E9E9E;
+    border:none;
+    background:white;
+    border-radius: 6px;
+}
+</style>
